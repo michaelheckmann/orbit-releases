09b6ca4b1cee80d0c9adca93cf7229ce5c0adad2
michaelheckmann <heckmannmichael@yahoo.de>
2025-10-29 16:13:30 +0100

    fix og image

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://orbit.heckmann.app",
+  site: "https://reachorbit.app",
   adapter: cloudflare({
     imageService: "compile",
     platformProxy: {
